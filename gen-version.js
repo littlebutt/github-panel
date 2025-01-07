b4fe89d1e97d01c0a7894b6fd609d4381242f3d6
littlebutt <luogan199686@gmail.com>
2025-01-07 21:50:28 +0800

    chore: Add release-win action

---
+++ gen-version.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require("fs")
+
+fs.readFile('./package.json', 'utf-8', (err, data) => {
+    if (err) {
+        return
+    }
+    const packageJson = JSON.parse(data)
+    fs.writeFile('./VERSION', `v${packageJson.version}`, () => {
+        console.log("VERSION generated!")
+    })
+})
